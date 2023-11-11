a4fc68f67f3c14099de361b9b035c39ceca11bbc
evgueniazet <zelenkoea19@gmail.com>
2023-11-11 22:08:10 +0300

    feat: add unit  tests for Button and Loader components

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,10 +3,10 @@ import styles from "./Loader.module.scss";
 
 const Loader: React.FC = () => {
   return (
-    <div className={styles.loaderWrapper}>
+    <div className={styles.loaderWrapper} data-testid="loaderWrapper">
       <div className={styles.loader}>
         {Array.from({ length: 6 }, (_, index) => (
-          <div key={index} className={styles.loaderItem}></div>
+          <div key={index} className={styles.loaderItem} data-testid="loaderItem"></div>
         ))}
       </div>
     </div>
